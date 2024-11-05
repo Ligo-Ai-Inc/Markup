3dbbc5f1348f2b46e1817282ad28449ac3e1b184
AnNT <ann52999@gmail.com>
2024-11-05 22:11:45 +0700

    update

--- subpages/config.py
+++ subpages/config.py
@@ -12,6 +12,10 @@ if "configured" not in st.session_state:
     st.session_state.configured = False
 if "template" not in st.session_state:
     st.session_state.template = None
+if "picture" not in st.session_state:
+    st.session_state.picture = None
+if "org_picture" not in st.session_state:
+    st.session_state.org_picture = None
 
 nrow = st.number_input("Number of rows", min_value=1, max_value=5, value=5)
 st.session_state.nrow = nrow
@@ -24,31 +28,35 @@ if configure:
     st.session_state.configured = False
     st.session_state.template = None
     st.session_state.points = []
+    st.session_state.picture = None
+    st.session_state.org_picture = None
 
 if not st.session_state.configured:
 
     img_file_buffer = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if img_file_buffer is not None:
-        org_picture = Image.open(img_file_buffer)
-        org_picture = ImageOps.exif_transpose(org_picture)
-        data = list(org_picture.getdata())
-        org_picture_without_exif = Image.new(org_picture.mode, org_picture.size)
-        org_picture_without_exif.putdata(data)
-        org_picture = org_picture_without_exif
+        if st.session_state.picture is None:
+            org_picture = Image.open(img_file_buffer)
+            org_picture = ImageOps.exif_transpose(org_picture)
+            data = list(org_picture.getdata())
+            org_picture_without_exif = Image.new(org_picture.mode, org_picture.size)
+            org_picture_without_exif.putdata(data)
+            org_picture = org_picture_without_exif
+            org_picture = np.array(org_picture)
+            st.session_state.org_picture = org_picture
+            picture = org_picture.copy()
+            scale_factor = 1080 / org_picture.shape[1]
+            picture = cv2.resize(picture, (1080, int(picture.shape[0] * scale_factor)))
+            st.session_state.picture = picture
 
-        org_picture = np.array(org_picture)
-        picture = org_picture.copy()
-        scale_factor = 1080 / org_picture.shape[1]
-        picture = cv2.resize(picture, (1080, int(picture.shape[0] * scale_factor)))
-
-        vis_points = np.array([[int(x*picture.shape[1]), int(y*picture.shape[0])] for x, y in st.session_state.points]).astype(np.int32)
+        vis_points = np.array([[int(x*st.session_state.picture.shape[1]), int(y*st.session_state.picture.shape[0])] for x, y in st.session_state.points]).astype(np.int32)
         vis_points = vis_points.reshape((-1, 1, 2))
         if len(vis_points) > 1:
-            picture = cv2.polylines(picture, [vis_points], isClosed=True, color=(0, 255, 0), thickness=10)
+            st.session_state.picture = cv2.polylines(st.session_state.picture, [vis_points], isClosed=True, color=(0, 255, 0), thickness=10)
 
         value = streamlit_image_coordinates(
-            picture,
+            st.session_state.picture,
             use_column_width=True,
         )
 
@@ -67,13 +75,13 @@ if not st.session_state.configured:
             w = 640
             h = 400
 
-        srcs = np.array([[int(x*org_picture.shape[1]), int(y*org_picture.shape[0])] for x, y in st.session_state.points]).astype(np.int32)
+        srcs = np.array([[int(x*st.session_state.org_picture.shape[1]), int(y*st.session_state.org_picture.shape[0])] for x, y in st.session_state.points]).astype(np.int32)
         srcs = srcs.reshape((-1, 1, 2))
         dsts = np.array([[0, 0], [w, 0], [w, h], [0, h]]).astype(np.int32)
 
         if len(srcs) == 4:
             M = cv2.getPerspectiveTransform(srcs.astype(np.float32), dsts.astype(np.float32))
-            res_img = cv2.warpPerspective(org_picture, M, (w, h))
+            res_img = cv2.warpPerspective(st.session_state.org_picture, M, (w, h))
             st.session_state.configured = True
             st.session_state.template = res_img
             st.rerun()
